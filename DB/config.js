9dbab057d1bef7bf2d31ce4a57666c63c04229ab
Dhiraj <dhiraj.mobicloud@gmail.com>
2023-01-25 17:23:50 +0530

    added cors package, gitignore file

--- DB/config.js
+++ DB/config.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose')
 
-// const  Connect =()=>{
-//   
-//   return  mongoose.connect('mongodb://localhost:27017/Vehicle_Details', ()=>{
-//     console.log('Connection Successfull')
-//   })
-// }
 const Connect = () => {
     // return mongoose.connect('mongodb+srv://dhirajmobicloud:Mobi%40123@cluster0.u1kcqe0.mongodb.net/messes');
     return mongoose.connect('mongodb+srv://dhirajmobicloud:Mobi%40123@cluster0.u1kcqe0.mongodb.net/test')
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const Connect = require('./DB/config');
 const mongoose = require('mongoose')
+const cors = require('cors')
 const surface_RH_139_defects = require('./Routes/Surface_RH_139/fetching_surface_rh_139_defects')
 const add_surface_RH_139_defect = require('./Routes/Surface_RH_139/AddNew_surface_rh_139_defect')
 const surface_FTR_139_defects = require('./Routes/Surface FTR 139/fetching_surface_ftr_139_defects')
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 
 app.use('/surface_RH_139_defects', surface_RH_139_defects);
 app.use('/add_surface_RH_139_defect', add_surface_RH_139_defect);
